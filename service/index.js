a3ad3531d7110fbf46ef534e031144a2f94237a2
Jason Panzer <jason.drew.panzer@gmail.com>
2021-01-31 01:45:24 -0800

    Events route

--- service/index.js
+++ service/index.js
@@ -2,55 +2,16 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
-const productsRouter = require('./routes/products');
+const eventsRouter = require('./routes/eventsRouter');
+const morgan = require('morgan');
 
-// a logging middleware
-const logger = (req, res, next) => {
-  console.log(`App received request ${req.method} - ${req.url}`);
-  return next();
-}
-
-const addSomething = (req, res, next) => {
-  req.newValue = "something valueable";
-  // what happens when we neglect to add next()
-  return next();
-}
-
-app.use(logger);
-app.use(addSomething)
-app.use(express.json());
-// alternatively, app.use('/api/v4', [logger, addSomething, express.json()]);
+app.use(morgan('tiny'));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// Get all products
-app.get('/api/v1/products', (req, res) => {
-  res.sendStatus(501);
-});
-
-// Get a single product by id
-app.get('/api/v1/products/:id', (req, res) => {
-  res.sendStatus(501);
-});
-
-// Create a new product
-app.post('/api/v1/products', (req, res) => {
-  res.sendStatus(501);
-});
-
-// Update an existing product
-app.put('/api/v1/products/:id', (req, res) => {
-  res.sendStatus(501);
-});
-
-// Delete an existing product
-app.delete('/api/v1/products/:id', (req, res) => {
-  res.sendStatus(501);
-});
-
-app.use('/api/v2/products', productsRouter);
+app.use('/v1/events', eventsRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
