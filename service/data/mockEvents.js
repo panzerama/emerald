470fbbc9ef699d7ab28004b81f49373b9367ee00
Jason Panzer <jason.drew.panzer@gmail.com>
2021-01-31 08:58:40 -0800

    Adding event routes and mock events

---
+++ service/data/mockEvents.js
@@ -0,0 +1,44 @@
+exports.upcomingEvents = [
+  {
+    name: "Love is in the Aaracokra",
+    date: "2021-02-15T18:33:27-08:00",
+    gameMaster: "Jason D Panzer",
+    location: "Seattle, WA",
+    details: "A crazy bird-brained adventure",
+  },
+  {
+    name: "Jurassic Park",
+    date: "2021-02-22T18:33:27-08:00",
+    gameMaster: "Allison Smith",
+    location: "Seattle, WA",
+    details: "Dinosaurs",
+  },
+  {
+    name: "Saved by the Spell",
+    date: "2021-03-10T19:33:27-08:00",
+    gameMaster: "Nikkie Silveira",
+    location: "Bothell, WA",
+    details: "High school is dark magic",
+  },
+  {
+    name: "The Land of the Midnight Sun",
+    date: "2021-03-28T15:33:27-08:00",
+    gameMaster: "Doug Johnson",
+    location: "Olympia, WA",
+    details: "Vikings run amok",
+  },
+  {
+    name: "Love is in the Aaracokra",
+    date: "2021-02-15T18:33:27-08:00",
+    gameMaster: "Jason D Panzer",
+    location: "Seattle, WA",
+    details: "A crazy bird-brained adventure",
+  },
+  {
+    name: "Love is in the Aaracokra",
+    date: "2021-02-15T18:33:27-08:00",
+    gameMaster: "Jason D Panzer",
+    location: "Seattle, WA",
+    details: "A crazy bird-brained adventure",
+  },
+];
---
+++ service/routes/eventsRouter.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const eventsRouter = express.Router();
+
+const { upcomingEvents } = require('../data/mockEvents');
+
+eventsRouter
+  .route('/')
+  .get((req, res) => {
+    res.send(upcomingEvents);
+  })
+
+module.exports = eventsRouter;
