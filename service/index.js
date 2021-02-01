3481d8a56dc93e1c42367def73ade3e34ffa7894
Jason Panzer <jason.drew.panzer@gmail.com>
2021-01-31 19:32:58 -0800

    hooked up posts router

--- service/index.js
+++ service/index.js
@@ -5,15 +5,13 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 const eventsRouter = require('./routes/eventsRouter');
+const postsRouter = require('./routes/postsRouter');
 
 app.use(morgan('tiny'));
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.use('/v1/events', eventsRouter);
+app.use('/v1/posts', postsRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ service/routes/postsRouter.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const postsRouter = express.Router();
+
+const posts = require("../data/mockPosts");
+
+postsRouter.route("/").get((req, res) => {
+  res.json(posts);
+});
+
+module.exports = postsRouter;
