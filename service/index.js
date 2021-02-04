bd3d34c79d2c44b4292b2adeb89349271df727f1
Jason Panzer <jason.panzer@nordstrom.com>
2021-02-03 17:56:35 -0800

    Database connection

--- service/index.js
+++ service/index.js
@@ -4,6 +4,17 @@ const port = 4000;
 const morgan = require('morgan');
 const cors = require('cors');
 
+// Setting up mongoose connection
+// Example code from https://mzl.la/39Ngmr4
+var mongoose = require('mongoose');
+var mongoDB = 'insert_your_database_url_here';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Retain an instance of the connection so that we can log errors
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('close', () => { console.log("MongoDB connection closed") });
+
 const eventsRouter = require('./routes/eventsRouter');
 const postsRouter = require('./routes/postsRouter');
 
