59d69ca7fa898faaf8900053688dedb8a06a11cf
Jason Panzer <jason.panzer@nordstrom.com>
2021-02-16 20:25:43 -0800

    Adding linting command and comments to custom auth0 provider component

--- client/src/components/Auth/AuthProviderWithHistory.js
+++ client/src/components/Auth/AuthProviderWithHistory.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+/* This component is borrowed from
+https://auth0.com/blog/complete-guide-to-react-user-authentication/ */
 const AuthProviderWithHistory = ({ children }) => {
   const domain = process.env.REACT_APP_AUTH_0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH_0_CLIENT_ID;
 
   const history = useHistory();
-
+  /* The blog example mentioned above confusingly names this variable appState,
+     which in fact does not represent React state, just Auth0 provider state */
   const onRedirectCallback = (appState) => {
     history.push(appState?.returnTo || window.location.pathname);
   };
